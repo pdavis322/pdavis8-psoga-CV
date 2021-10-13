c37d8b97e270e7a3965462d8274a2e33693fdc44
Patrick <psoga117@gmail.com>
2021-10-12 18:58:25 -0700

    line detection

--- edges.py
+++ edges.py
@@ -5,30 +5,56 @@ from shi_tomasi import shi_tomasi
 from math import ceil
 
 
+def _fix_negative_rho_in_hesse_normal_form(lines: np.ndarray) -> np.ndarray:
+    lines = lines.copy()
+    neg_rho_mask = lines[..., 0] < 0
+    lines[neg_rho_mask, 0] = - \
+        lines[neg_rho_mask, 0]
+    lines[neg_rho_mask, 1] =  \
+        lines[neg_rho_mask, 1] - np.pi
+    return lines
+
+
 def hough(img):
-    lines = cv2.HoughLines(img, 1, np.pi/180, 250)
+    lines = cv2.HoughLines(img, 1, np.pi/360, 150)
+    # lines = _fix_negative_rho_in_hesse_normal_form(lines)
+
+    # lines = cv2.HoughLinesP(img, 1, np.pi/180, 130, 30, 50)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     slopes = {}
+    length = np.sqrt(img.shape[0]**2 + img.shape[1]**2)
+    # strong_lines = [lines[0]]
+    # for line in lines[1:]:
+    #     for strong_line in strong_lines:
+    #         print(strong_line)
+    #         strong_rho, strong_theta = strong_line
+    #         rho, theta = line
+    #         if abs(strong_rho - rho) <= 100 and abs(strong_theta - theta) <= 20:
+    #             strong_lines.append(line)
+
     for i, line in enumerate(lines):
         for rho, theta in line:
+            # for x1, y1, x2, y2 in line:
             a = np.cos(theta)
             b = np.sin(theta)
             x0 = a*rho
             y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
-            color = (0, 0, 255) if i != 4 else (0, 255, 0)
-            if x2 - x1 != 0:
-                s = ceil((y2 - y1) / (x2 - x1))
-                if s not in slopes:
-                    slopes[s] = i
-                else:
-                    print(f'{i}, {slopes[s]} are duplicates')
-                    cv2.line(img, (x1, y1), (x2, y2), color, 2)
-            img = cv2.putText(img, str(
-                i), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA, False)
+            x1 = int(x0 + length*(-b))
+            y1 = int(y0 + length*(a))
+            x2 = int(x0 - length*(-b))
+            y2 = int(y0 - length*(a))
+            color = (0, 0, 255)
+            if x2 - x1 == 0 or y1 < ceil(0.25*img.shape[0]) or x2 < ceil(0.25*img.shape[1]):
+                continue
+            s = ceil((y2 - y1) / (x2 - x1))
+            if s not in slopes:
+                slopes[s] = i
+            else:
+                print(f'{i}, {slopes[s]} are duplicates')
+                cv2.line(img, (x1, y1), (x2, y2), color, 2)
+            # img = cv2.putText(img, str(
+            #     i), (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA, False)
+
     print(slopes)
 
     return img
@@ -57,19 +83,20 @@ def floodfill(img):
 
 
 def morph(img):
-    ret1, binary_image = cv2.threshold(
-        img, 0, 150, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    blurred = cv2.GaussianBlur(img, (7, 7), 0)
+    # ret1, binary_image = cv2.threshold(
+    #     img, 0, 150, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # img = cv2.GaussianBlur(img, (3, 3), 0)
     # binary_image = cv2.adaptiveThreshold(
     #     blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 8)
     kernel = np.ones((2, 2), np.uint8)
-    # img = floodfill(binary_image)
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    # img = floodfill(img)
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    # img = cv2.dilate(img, kernel, iterations=1)
+    # img = cv2.erode(img, kernel, iterations=30)
+    # img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
-    return binary_image
+    return img
 
 
 def get_contours(img):
@@ -94,27 +121,26 @@ def get_contours(img):
 without_magnus_white_shirt = 'without_magnus_white_shirt.png'
 chessboard = 'chessboard.png'
 boy_vs_man = 'boy_vs_man.png'
-initial_state = 'initial1.PNG'
+initial1 = 'initial1.PNG'
+initial2 = 'initial2.png'
 
-img = cv2.imread(initial_state)
-'''
-scale_percent = 50
-img = cv2.resize(img, (int(img.shape[1] * scale_percent / 100), int(img.shape[0] * scale_percent / 100)), interpolation = cv2.INTER_AREA)
-'''
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.imread(initial1)
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Before morph operations', img)
 
-morphed_img = morph(gray)
-#cv2.imshow('After morph operations', morphed_img)
+morphed_img = morph(img)
+cv2.imshow('After morph operations', morphed_img)
 
-contour_rect = get_contours(morphed_img)
-print(contour_rect)
-x, y, w, h = contour_rect
+# contour_rect = get_contours(morphed_img)
+# print(contour_rect)
+# x, y, w, h = contour_rect
 
-cropped = img[y:y + h, x:x + w]
+# cropped = img[y:y + h, x:x + w]
 #cv2.imshow('Cropped', cropped)
 
-canny_img = cv2.Canny(gray, 250, 300, apertureSize=3)
+
+# canny_img = cv2.Canny(morphed_img, 500, 550, apertureSize=3)
+canny_img = cv2.Canny(img, 200, 250, apertureSize=3)
 cv2.imshow('Canny', canny_img)
 img = hough(canny_img)
 cv2.imshow('Hough', img)
